cda5bcadcca7778179600ca4312bc1e2f9f5dcc6
Serhii Bura <serhiibura@gmail.com>
2020-05-19 00:39:19 +0300

    backend: [Events] add new events and signals

--- backend/include/qp/backend_events.hpp
+++ backend/include/qp/backend_events.hpp
@@ -1,8 +1,13 @@
 #pragma once
 #include <string>
+#include <cstdint>
 #include "qpcpp.hpp"
+#include "database/database_manager.hpp"
 
-enum {
+#define MQTT_MESSAGE_SIZE 500
+#define MQTT_MESSAGE_DATA_SIZE 255
+
+enum eSignals{
     MQTT_MESSAGE_RECIVED_SIG = QP::Q_USER_SIG,
     HW_UPDATE_SENSOR_SIG,
     HW_ADD_SENSOR_SIG,
@@ -15,6 +20,10 @@ enum {
     DB_CB_ADD_SENSOR_SIG,
     DB_ERR_SIG,
     TIMEOUT_SIG,
+    MQTT_START_LOOP_SIG,
+    SYSTEM_ALLARM_SIG,
+    MAX_PUB_SIG,
+    HANDLING_FINISHED_SIG,
 };
 
 enum {ALLARM_TICKS = 100};
@@ -24,4 +33,27 @@ public:
     std::string m_topic;
     std::string m_message;
     MQTTMessageRecived(QP::QSignal sig);
+};
+class HWAddSensor : public QP::QEvt {
+public:
+    sensor_t m_type;
+    std::string m_title;
+public:
+    HWAddSensor(QP::QSignal sig);
+};
+
+class HWSensorValueUpdate : public QP::QEvt {
+public:
+    int32_t m_id;
+    int32_t m_value;
+
+public:
+    HWSensorValueUpdate(QP::QSignal sig);
+};
+class HWRemoveSensor : public QP::QEvt {
+public:
+    uint8_t m_id;
+
+public:
+    HWRemoveSensor(QP::QSignal sig);
 };
--- backend/sources/qp/backend_events.cpp
+++ backend/sources/qp/backend_events.cpp
@@ -1,7 +1,20 @@
 #include "qp/backend_events.hpp"
+
 Q_DEFINE_THIS_FILE
 
-MQTTMessageRecived::MQTTMessageRecived(QP::QSignal _sig)
-        :QEvt(){
-                this->sig = _sig;
-        }
+MQTTMessageRecived::MQTTMessageRecived(QP::QSignal _sig) 
+: QEvt() {
+    this->sig = _sig;
+}
+HWAddSensor::HWAddSensor(QP::QSignal _sig) 
+: QEvt() { 
+    this->sig = _sig; 
+}
+HWSensorValueUpdate::HWSensorValueUpdate(QP::QSignal _sig) 
+: QEvt() { 
+    this->sig = _sig; 
+}
+HWRemoveSensor::HWRemoveSensor(QP::QSignal _sig)
+: QEvt() { 
+    this->sig = _sig; 
+}
