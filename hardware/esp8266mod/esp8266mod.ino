b6834465b30faa65be3ea22f949753a716566358
Serhii Bura <serhiibura@gmail.com>
2020-05-21 02:09:11 +0300

    hardware: Common Sensor api class implementation

--- hardware/esp8266mod/esp8266mod.ino
+++ hardware/esp8266mod/esp8266mod.ino
@@ -5,12 +5,13 @@
 #include "Adafruit_BME280.h"
 #include "Adafruit_Sensor.h"
 #include "Wire.h"
-#include <SPI.h>
+#include "SPI.h"
+#include "lib/csensor_api.hpp"
 
 
 //------------------WIFI-SETTINGS-------------------------------------------------------------------
-#define STASSID "ACCESS_POINT"  //Enter your access point ssid
-#define STAPSK  "PASSWORD"      //Enter access point password
+#define STASSID "Pentagon"  //Enter your access point ssid
+#define STAPSK  "venividivici228_JP"      //Enter access point password
 
 //------------------MQTT-SETTINGS-------------------------------------------------------------------
 #define MQTT_SERVER "localhost"
@@ -18,7 +19,7 @@
 
 //------------------BME-SETTINGS-------------------------------------------------------------------
 #define SEALEVELPRESSURE_HPA (1013.25)
-#define SENSORS_COUNT 3//Enter your sensors count
+#define SENSORS_COUNT 9 //Enter your sensors count
 #define VALUE_PRESCALLER 100
 
 // assign the ESP8266 pins to arduino pins
@@ -36,74 +37,64 @@
 #define BME2_CS D4
 #define BME3_CS D6
 
+Adafruit_BME280 g_bme1(BME1_CS, BME_MOSI, BME_MISO, BME_SCK);
+Adafruit_BME280 g_bme2(BME2_CS, BME_MOSI, BME_MISO, BME_SCK);
+Adafruit_BME280 g_bme3(BME3_CS, BME_MOSI, BME_MISO, BME_SCK);
 
-#define SEALEVELPRESSURE_HPA (1013.25)
+CSensor g_sensors[SENSORS_COUNT]; //Vector of sensors
 
-typedef enum {
-    no_respond,
-    active_state,
-}sensor_status_t;
+void s_getBme1Temperature(int& value){
+  value = (int)(g_bme1.readTemperature() * (VALUE_PRESCALLER));
+}
+void s_getBme1Humidity(int& value){
+  value = (int)(g_bme1.readHumidity() * (VALUE_PRESCALLER));
+}
+void s_getBme1Pressure(int& value){
+  value = (int)(g_bme1.readPressure() * (VALUE_PRESCALLER));
+}
 
-struct mySensor_t{
-    uint8_t id;
-    long int tempValue;
-    long int humValue;
-    long int presValue;
-    Adafruit_BME280* sensorInst;
-    sensor_status_t sensorStatus;
-};
+void s_getBme2Temperature(int& value){
+  value = (int)(g_bme2.readTemperature() * (VALUE_PRESCALLER));
+}
+void s_getBme2Humidity(int& value){
+  value = (int)(g_bme2.readHumidity() * (VALUE_PRESCALLER));
+}
+void s_getBme2Pressure(int& value){
+  value = (int)(g_bme2.readPressure() * (VALUE_PRESCALLER));
+}
 
-
-Adafruit_BME280 bme1(BME1_CS, BME_MOSI, BME_MISO, BME_SCK);
-Adafruit_BME280 bme2(BME2_CS, BME_MOSI, BME_MISO, BME_SCK);
-Adafruit_BME280 bme3(BME3_CS, BME_MOSI, BME_MISO, BME_SCK);
-
-
-static Adafruit_BME280* g_bmeRef[] = {&bme1, &bme2, &bme3}; //Vector of sensors
-static mySensor_t g_sensors[SENSORS_COUNT]; //Vector of sensors
-
-static void s_monitorValues() {
-    for (int sensId = 0; sensId < SENSORS_COUNT; ++sensId) {
-        if(g_sensors[sensId].sensorStatus == active_state) { 
-            g_sensors[sensId].tempValue = (long int)(g_sensors[sensId].sensorInst->readTemperature() * (VALUE_PRESCALLER));
-            g_sensors[sensId].humValue = (long int)(g_sensors[sensId].sensorInst->readHumidity() * (VALUE_PRESCALLER));
-            g_sensors[sensId].presValue = (long int)(g_sensors[sensId].sensorInst->readPressure());
-        }
-    }
+void s_getBme3Temperature(int& value){
+  value = (int)(g_bme3.readTemperature() * (VALUE_PRESCALLER));
+}
+void s_getBme3Humidity(int& value){
+  value = (int)(g_bme3.readHumidity() * (VALUE_PRESCALLER));
+}
+void s_getBme3Pressure(int& value){
+  value = (int)(g_bme3.readPressure() * (VALUE_PRESCALLER));
 }
 
 static void s_printValues() {
     for (int sensId = 0; sensId < SENSORS_COUNT; ++sensId) {
-        if(g_sensors[sensId].sensorStatus == active_state) {       
+        if(g_sensors[sensId].getStatus() == active_state) {       
             Serial.println("");
             Serial.print("{");
-            Serial.print(g_sensors[sensId].id);
+            Serial.print(g_sensors[sensId].getId());
             Serial.print("} ");
-            Serial.print("Temperature");
-            Serial.print("{");
-            Serial.print(g_sensors[sensId].tempValue);
-            Serial.print("} ");
-            Serial.print("Humidity");
-            Serial.print("{");
-            Serial.print(g_sensors[sensId].humValue);
-            Serial.print("} ");
-            Serial.print("Pressure");
-            Serial.print("{");
-            Serial.print(g_sensors[sensId].presValue);
+            Serial.print(sensorTypeToString(g_sensors[sensId].getType()));
+            Serial.print(" {");
+            Serial.print(g_sensors[sensId].getValue());
             Serial.print("} ");
         }
     }
 }
 
-void setup() {
-    Serial.begin(115200);
-    //------------------------------WIFI-CONFIGURATION---------------------------------------------------
+static void s_wifiSetup(const char* ssid, const char* password) {
     Serial.println();
     Serial.println();
     Serial.print("Connecting to ");
     Serial.println(ssid);
     WiFi.mode(WIFI_STA);
-    WiFi.begin(STASSID, STAPSK);
+    WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
@@ -112,23 +103,53 @@ void setup() {
     Serial.println("WiFi connected");
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
-    
-    //------------------------------SENSORS-CONFIGURATION---------------------------------------------------
-    for (size_t i = 0; i < SENSORS_COUNT; i++) {
-        g_sensors[i].id = i;
-        g_sensors[i].sensorInst = g_bmeRef[i];
-        g_sensors[i].sensorStatus = (sensor_status_t)(g_sensors[i].sensorInst->begin());
-    }
+}
+
+static void s_sensorsConfiguration() {
+    g_sensors[0].setType(temperature);
+    g_sensors[0].setCallback(s_getBme1Temperature);
+    g_sensors[0].setStatus((sensorStatus)g_bme1.begin());
+    g_sensors[1].setType(humidity);
+    g_sensors[1].setCallback(s_getBme1Humidity);
+    g_sensors[1].setStatus((sensorStatus)g_bme1.begin());
+    g_sensors[2].setType(pressure);
+    g_sensors[2].setCallback(s_getBme1Pressure);
+    g_sensors[2].setStatus((sensorStatus)g_bme1.begin());
+
+    g_sensors[3].setType(temperature);
+    g_sensors[3].setCallback(s_getBme2Temperature);
+    g_sensors[3].setStatus((sensorStatus)g_bme2.begin());
+    g_sensors[4].setType(humidity);
+    g_sensors[4].setCallback(s_getBme2Humidity);
+    g_sensors[4].setStatus((sensorStatus)g_bme2.begin());
+    g_sensors[5].setType(pressure);
+    g_sensors[5].setCallback(s_getBme2Pressure);
+    g_sensors[5].setStatus((sensorStatus)g_bme2.begin());
+
+    g_sensors[6].setType(temperature);
+    g_sensors[6].setCallback(s_getBme3Temperature);
+    g_sensors[6].setStatus((sensorStatus)g_bme3.begin());
+    g_sensors[7].setType(humidity);
+    g_sensors[7].setCallback(s_getBme3Humidity);
+    g_sensors[7].setStatus((sensorStatus)g_bme3.begin());
+    g_sensors[8].setType(pressure);
+    g_sensors[8].setCallback(s_getBme3Pressure);
+    g_sensors[8].setStatus((sensorStatus)g_bme3.begin());
+}
+
+void setup() {
+    Serial.begin(115200);
+    s_wifiSetup(STASSID, STAPSK);
+    s_sensorsConfiguration();
 }
 
 void loop() {
     Serial.print("connecting to ");
-    Serial.print(host);
+    Serial.print(MQTT_SERVER);
     Serial.print(':');
-    Serial.println(port);
+    Serial.println(MQTT_SERVER_PORT);
     WiFiClient wifiClient;
-    PubSubClient client(mqtt_server, 1883, wifiClient); // 1883 is the listener port for the Broker
-    s_monitorValues();
+    PubSubClient client(MQTT_SERVER, MQTT_SERVER_PORT, wifiClient); // 1883 is the listener port for the Broker
     s_printValues();
     delay(1000);
 }
