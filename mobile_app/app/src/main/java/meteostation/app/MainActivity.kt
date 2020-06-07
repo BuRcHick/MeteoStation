7c1f9e15627180651771c057916613383ada88d7
Serhii Bura <serhiibura@gmail.com>
2020-06-07 23:00:42 +0300

    mobile_app: MQTT Listner func implementation

--- mobile_app/app/src/main/java/meteostation/app/MainActivity.kt
+++ mobile_app/app/src/main/java/meteostation/app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.text.format.Formatter
+import android.util.JsonReader
 import android.util.Log
 import android.view.Menu
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -16,58 +17,33 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+
 import androidx.appcompat.widget.Toolbar
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
+import meteostation.app.mqttclient.MQTTClient
+import meteostation.app.mqttclient.MqttListner
+import meteostation.app.sensormanager.Sensor
+import meteostation.app.sensormanager.SensorManager
+import meteostation.app.sensormanager.eSensorType
+import org.json.JSONArray
+import org.json.JSONObject
 import java.net.InetAddress
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MqttListner {
+
+    private val mqtt_subscribed_topics = arrayOf("SESNORS_LIST", "SENSOR_UPDATE", "LAST_SENSOR")
+    private var sensorManager = SensorManager()
+    private var mqttClient = MQTTClient(this)
+    private var isInit = false
+    private var isConnect = false
+    private var uiSensors = ArrayList<String>()
+
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private var mqttClient = MQTTClient()
-
-    fun connectToMqttBroker(){
-        CoroutineScope(IO).launch {
-            var list = ArrayList<String>()
-            val wm: WifiManager? = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager?
-            if(wm != null){
-                var ip: String? = Formatter.formatIpAddress(wm!!.connectionInfo.ipAddress)
-                if(ip != null) {
-                    val pref = ip.substring(0, ip.lastIndexOf('.') + 1)
-                    async {
-                        for (i in 0..255) {
-                            async {
-                                val newIp = pref + i.toString()
-                                if(newIp != ip) {
-                                    val address = InetAddress.getByName(newIp)
-                                    try {
-                                        if(address.isReachable(1000)) {
-                                            list.add(address.hostAddress)
-                                        } else {
-                                        }
-                                    } catch (e: Exception){
-                                    }
-                                }
-                            }
-                        }
-                    }.await()
-                }
-            }
-            val channel = Channel<Boolean>()
-            list.forEach{
-                mqttClient.createMqttClient(applicationContext, it, "MeteostationAndroid")
-                Log.d("METEOSTATION", "Truing to connect to: $it")
-                mqttClient.connect("tcp://" + it + ":1883", "AndroidID", applicationContext, channel)
-                if(channel.receive()) {
-                    return@launch
-                }
-            }
-        }
-
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -89,7 +65,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-        connectToMqttBroker()
+        connectToMqttBroker(applicationContext)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -101,5 +77,92 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun connectToMqttBroker(context: Context){
+        CoroutineScope(Dispatchers.IO).launch {
+            var list = ArrayList<String>()
+            val wm: WifiManager? = context.getSystemService(Context.WIFI_SERVICE) as WifiManager?
+            if(wm != null){
+                var ip: String? = Formatter.formatIpAddress(wm!!.connectionInfo.ipAddress)
+                if(ip != null) {
+                    val pref = ip.substring(0, ip.lastIndexOf('.') + 1)
+                    async {
+                        for (i in 0..255) {
+                            async {
+                                val newIp = pref + i.toString()
+                                if(newIp != ip) {
+                                    val address = InetAddress.getByName(newIp)
+                                    try {
+                                        if(address.isReachable(1000)) {
+                                            Log.d("SUCCES", newIp)
+                                            list.add(address.hostAddress)
+                                        } else {
+                                        }
+                                    } catch (e: Exception){
+                                    }
+                                }
+                            }
+                        }
+                    }.await()
+                    val channel = Channel<Boolean>()
+                    list.forEach{
+                        Log.d("METEOSTATION", "Truing to connect to: $it")
+                        mqttClient.createClient("tcp://" + it + ":1883", "Android", context)
+                        mqttClient.connect(it, channel)
+                        if(channel.receive() == true) {
+                            for (topic in mqtt_subscribed_topics){
+                                mqttClient.subscribe(topic, channel)
+                                if(channel.receive()){
+
+                                }
+                            }
+                            channel.close()
+                            getSensors()
+                            return@launch
+                        }
+                    }
+                    channel.close()
+                }
+            }
+        }
+
+    }
+
+    override fun onMessageRecived(topic: String, message: String) {
+        if(isConnect) {
+            //Log.d("=========$topic===========", message)
+            when (topic) {
+                "SESNORS_LIST" -> {
+                    if(isInit) {
+                        isInit = false
+                        sensorManager.clear()
+                    }
+                    var array = JSONArray(message)
+                    var jsonSensor = JSONObject(array.get(0).toString())
+                    var sensor = Sensor(jsonSensor.getInt("m_id"), jsonSensor.getString("m_type"), jsonSensor.getInt("m_status"), jsonSensor.getInt("m_value"))
+                    sensorManager.addSensor(sensor)
+                    if(uiSensors.find { it == sensor.m_type } == null) {
+                        uiSensors.add(sensor.m_type)
+                    }
+
+                }
+                "LAST_SENSOR" -> {
+                    isInit = true
+                    Log.d("SENSOR_MANAGER", "==================${uiSensors.size}")
+
+                }
+                "UPDATE_SENSOR" -> {
+                    if(isInit) {
+                    }
+                }
+            }
+        }
+
+    }
+
+    fun getSensors() {
+        isConnect = true
+        mqttClient.publish("GET_SENSORS", "")
     }
 }
